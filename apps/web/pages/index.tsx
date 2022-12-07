7f33dbf6536ab4cb7f84cbbe3061ae4ce1a06ba8
Mads-Henrik Hegge-Olsen <66017942+madshegge@users.noreply.github.com>
2022-12-07 12:12:26 +0100

    ++

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -2,8 +2,10 @@ import { Button } from 'ui'
 
 export default function Web() {
     return (
-        <div className="flex h-screen w-screen items-center justify-center">
-            <h1 className="text-blue-400">Web</h1>
+        <div className="flex flex-col items-center justify-center w-screen h-screen gap-6">
+            <h1 className="text-5xl text-gray-700">
+                This is a title within <kbd>"apps/web"</kbd>
+            </h1>
             <Button />
         </div>
     )
--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react'
 import './styles.css'
 export const Button = () => {
-    return <button className="bg-blue-400 p-4">Boop</button>
+    return (
+        <button className="p-4 px-4 py-2 text-white transition-all ease-in-out bg-blue-500 rounded-md shadow-md outline-2 outline-offset-2 outline-blue-500 focus:outline focus:ease-in-out">
+            This is a button from "ui"
+        </button>
+    )
 }
